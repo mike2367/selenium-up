b2f932c5f3c447b31a316b76b78de6b9c65aa64f
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-11-07 10:21:39 +0800

    first commit

---
+++ Connection.py
@@ -0,0 +1,70 @@
+import random
+from typing import Literal, Union
+from selenium import webdriver
+from main import logger
+import settings
+standard_option: list = [
+    "--user-agent={}".format(random.choice(settings.ua_list)),
+    "--incognito",
+    "--disable-gpu",
+    "--disable-blink-features",
+    "--disable-blink-features=AutomationControlled"
+]
+
+class Connection():
+    def __init__(self, 
+        selenium_driverType: Literal['Chrome', 'Firefox'] = 'Chrome',
+        DriverOption_param:Union[None, list] = None,
+        headless:bool = False         
+    ) -> None:
+        """
+        Initialize a Connection object with the provided parameters.
+
+        Parameters:
+        url (str): The URL to connect to.
+        selenium_driverType (Literal['Chrome', 'Firefox'], optional): The type of Selenium WebDriver to use. Defaults to 'Chrome'.
+        DriverOption_param (Union[None, list], optional): Additional options for the WebDriver. Defaults to None.
+        Headless (bool): Whether to run the WebDriver in headless mode, in here because regularly used. Defaults to False.
+        Returns:
+        None
+        """
+        self.selenium_driverType = selenium_driverType
+        self.DriverOption_param = DriverOption_param
+        self.headless = headless
+        self.opt_params = self.DriverOption_param + standard_option if self.DriverOption_param else standard_option
+        if headless:
+            self.opt_params.append("--headless")
+        # avoid repeated settings
+        self.opt_params = list(set(self.opt_params))
+        # for fingerprint elimination
+        with open('stealth.min.js', 'r') as f:
+            self.js = f.read()
+
+
+    def __repr__(self) -> str:
+        return f"""
+            WebDriver:{self.selenium_driverType}
+            DriverOptions:{self.opt_params}"""
+
+    @logger.catch
+    def driver_instance(self):
+        if self.selenium_driverType == 'Chrome':
+            options = webdriver.ChromeOptions()
+            options.binary_location = settings.CHROMIUM
+            for item in self.opt_params:
+                options.add_argument(item)
+            driver = webdriver.Chrome(options=options)
+
+
+        elif self.selenium_driverType == 'Firefox':
+            options = webdriver.FirefoxOptions()
+            options.binary_location = settings.FIREFOX
+            for item in self.opt_params:
+                options.add_argument(item)
+            driver = webdriver.Firefox(options=options)
+
+        if driver:
+            driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {'source': self.js})
+            logger.success("Selenium driver initialized")
+        return driver
+    
---
+++ Log.py
@@ -0,0 +1,63 @@
+from loguru import logger
+import yagmail
+import sys
+from notifiers.logging import NotificationHandler
+from typing import Union
+class Customized_Log:
+    log = logger
+    def __init__(self) -> None:
+        self.custom_logger = logger
+    
+    def log_setting(self, filepath:str = "./Log.log",
+                    rotation:str = "00:00",
+                    level:str = "DEBUG",
+                    Format:str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+                    email_level:str = None,
+                    contact_param:Union[dict, None] = None
+                    ) -> any:
+        """
+        set loguru logger setting.
+
+        Args:
+            filepath (str): The log file path. Defaults to "./Log.log".
+            rotation (str): The log file rotation time. Defaults to "00:00".
+            level (str): The log level. Defaults to "DEBUG".
+            Format (str): The log format string. Defaults to "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}".
+        """
+        self.custom_logger.add(filepath, rotation=rotation, level=level, format=Format)
+        self.email_setting(email_level, contact_param)
+        return self.custom_logger
+    def email_setting(self, level:str = "ERROR", param:Union[dict, None] = None) -> None:
+        """
+        Send email to the contact specified in the 'contact_param' dictionary once an error occurs.
+
+        The function checks if the 'contact_param' is provided and contains a valid email address.
+        If the conditions are met, it extracts the email domain from the username, creates a notification handler
+        using the 'notifiers' library, and adds this handler to the logger with an error level.
+
+        Returns:
+        None
+        """
+        if param:
+            try:
+                yag = yagmail.SMTP(user=param['username'], password=param['password'])
+                def send_email(message):
+                    yag.send(
+                        to=param['to'],
+                        subject='Log {} Notification'.format(level),
+                        contents=message
+                    )
+                
+                # Add the email sending function as a loguru handler
+                self.custom_logger.add(send_email, level=level)
+            except Exception as e:
+                logger.error(f"Failed to set up email handler: {e}")
+
+    def level_setting(self, level:str) -> None:
+        self.logger.remove()
+        """
+        Add a new terminal for higher level log processing
+        """
+        self.logger.add(sys.stdout, level=level)
+
+    
