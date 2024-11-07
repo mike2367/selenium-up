5cbacec6ae2449779a718396577b9b6bc7b606b0
mike2367 <yuguo2022@shanghaitech.edu.cn>
2024-11-07 22:53:10 +0800

    add csv service

--- SaveAction.py
+++ SaveAction.py
@@ -1,5 +1,30 @@
 from main import logger
+from typing import List, Union
+import csv
+import pymysql
+import pymongo
+import redis
 
 
 class SaveAction():
-    pass
+    def __init__(self) -> None:
+        pass
+    
+    @logger.catch
+    @staticmethod
+    def csv_save(filename:str, item_list:List[dict], header:list = None, encoding:str = 'utf-8')->None:
+        with open(filename, 'a', newline='', encoding=encoding) as f:
+            writer = csv.DictWriter(f, fieldnames=header)
+            if header:
+                writer.writeheader()
+            writer.writerows(item_list)
+        f.close()
+
+    def mysql_service(self)->None:
+        pass
+
+    def mongodb_service(self)->None:
+        pass
+
+    def redis_service(self)->None:
+        pass
