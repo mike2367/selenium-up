683e7092165c52a2f815143e8b64a9cd5fcd1ffb
mike2367 <yuguo2022@shanghaitech.edu.cn>
2025-01-24 09:50:17 +0800

    fix setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='seleniumUp',          # Required
-    version='0.0.2',           # Required
+    version='0.1.0',           # Required
     packages=find_packages(),  # Required
     include_package_data=True,  # Includes package data as specified below
     package_data={
